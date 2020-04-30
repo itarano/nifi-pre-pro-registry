ccd912e599657a81526aca06b46265989c3dcd60
Ignacio José Tárano Miranda <you@example.com>
2020-04-30 19:04:06 +0200

    create start script

---
+++ start.bat
@@ -0,0 +1,20 @@
+@echo off & setLocal
+
+for /f "delims=" %%x in (.env) do (set "%%x")
+
+set parameters="%*"
+if not x%parameters:registry=%==x%parameters% goto registry
+if "%*"=="" goto registry
+goto start-all
+
+:registry
+if exist volumes\nifi-registry\git\%REGISTRY_FLOW_STORAGE%\.git ( goto start-all ) else ( mkdir volumes\nifi-registry\git && cd volumes\nifi-registry\git && git clone https://github.com/itarano/%REGISTRY_FLOW_STORAGE%.git && cd ../../../ && goto start-all )
+
+:start-all
+docker-compose up -d %*
+goto done
+
+:done
+echo Done!
+
+endlocal
