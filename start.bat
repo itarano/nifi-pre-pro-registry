10c68f55d7dbe5a8436491f3a162299d42d42e57
Ignacio José Tárano Miranda <you@example.com>
2020-04-30 19:23:03 +0200

    update

--- start.bat
+++ start.bat
@@ -8,7 +8,8 @@ if "%*"=="" goto registry
 goto start-all
 
 :registry
-if exist volumes\nifi-registry\git\%REGISTRY_FLOW_STORAGE%\.git ( goto start-all ) else ( mkdir volumes\nifi-registry\git && cd volumes\nifi-registry\git && git clone https://github.com/itarano/%REGISTRY_FLOW_STORAGE%.git && cd ../../../ && goto start-all )
+if exist volumes\nifi-registry\git ( rmdir /Q /S volumes\nifi-registry\git )
+mkdir volumes\nifi-registry\git && cd volumes\nifi-registry\git && git clone https://github.com/itarano/%REGISTRY_FLOW_STORAGE%.git && cd ../../../ && goto start-all
 
 :start-all
 docker-compose up -d %*
