e5da6d947d373ceec8db095e1a5331b25b6995ba
Ignacio José Tárano Miranda <ignaciojose.tarano@gmail.com>
2020-05-21 20:27:24 +0200

    add start.py for wsl or linux

--- README.md
+++ README.md
@@ -16,6 +16,11 @@
 ## Estructura del proyecto
 - Fichero *.env* donde se recogen variables utilizadas en el fichero *docker-compose.yml*.
 - Fichero *docker-compose.yml* para definir el entorno.
-- Script *start.bat* para desplegar el entorno.
+- Script *start.bat* para desplegar el entorno en windows.
+- Script *start.py* para desplegar entorno (linux+python3). Se hace uso de un [CLI general](https://gist.github.com/itarano/bcfbc671072635acdce6e4fa444d65af) para docker compose en el que se puede implementar lógica antes de iniciar los contenedores. Por ejemplo, si en las variables de entorno *.env*, la variable que hace referencia a la url de la base de datos para guardar las transacciones de nifi-registry no es postgresql, no se inicia dicho servicio.
 - El directorio *volumes/nifi-registry* que comparte con el contenedor *registry* los drivers de PostgreSQL (necesarios en caso de configurar postgres en vez de h2 en el fichero *.env*).
 - El directorio donde se clona el repositorio en el que se almacenan las versiones de los flujos (en caso de elegir *git* en vez de *file* en el fichero *.env*). Dicho directorio está incluido en *.gitignore* y se creará de forma local (manual) clonando el repositorio donde queramos almacenar los flujos.
+
+## Deployment
+- Windows &rarr; *start.bat*
+- Linux o wsl 1 ([wsl 2](https://devblogs.microsoft.com/commandline/announcing-wsl-2/)) &rarr; *start.py*
---
+++ start.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import argparse
+import configparser
+
+import os
+import sys
+
+import yaml
+import subprocess
+
+# verified and get services from args command line
+def get_args_services(shell_services, compose_services):
+    docker_services = set()
+    if shell_services is not None:
+        for service in shell_services:
+            if not service in services:
+                print('Some of the specified service does not exist' + 
+                        '\nServices: ' + str(list(compose_services)))
+                sys.exit(1)
+            else:
+                docker_services.add(service)
+    return docker_services
+
+# get services from docker-compose.yml file
+def get_compose_services():
+    with open("docker-compose.yml", 'r') as stream:
+        try:
+            compose_services = set()
+            for service in yaml.safe_load(stream)['services'].keys():
+                compose_services.add(service)
+            return compose_services
+        except yaml.YAMLError as exc:
+            print(exc)
+
+def start(environment, shell_services, compose_services):
+    env = read_env('.env', environment)
+    services = get_args_services(shell_services, compose_services)
+
+    if not services:
+        services = compose_services
+    pre_up_do(services, env, environment)
+            
+    command = ("docker-compose.exe up --build -d {}".
+               format(str(services).replace('{', '').replace(',', '').replace('}', '')))
+    subprocess.call(command, shell=True)
+
+# own project and services logic!
+def pre_up_do(services, env, section):
+    if 'registry' in services:
+        if env.get(section, 'REGISTRY_FLOW_PROVIDER') == 'git':
+            #do
+            action_code = os.system("rm -rf volumes/nifi-registry/git && "
+                        "mkdir volumes/nifi-registry/git && "
+                        "cd volumes/nifi-registry/git && "
+                        "git clone https://github.com/{0}/{1}.git".
+                        format(env.get(section, 'REGISTRY_GIT_USER'), env.get(section, 'REGISTRY_FLOW_STORAGE')))
+            if action_code != 0:
+                print("Stoping deployment...")
+                sys.exit(1)
+
+        if 'postgresql' in env.get(section, 'REGISTRY_DB_URL'):
+            services.add('postgresql')
+        else:
+            services.remove('postgresql')
+            services.remove('pgadmin')
+
+def read_env(file, section):
+    settings = configparser.ConfigParser()
+    settings.read(file)
+    print('Using {0} properties...'.format(section))
+    return settings    
+
+services = get_compose_services()
+
+# Create the parser
+parser = argparse.ArgumentParser(description='Services: ' + str(list(services)))
+
+# Add the arguments
+parser.add_argument('env', metavar='environment', type=str,
+                    help='section environment to use')
+parser.add_argument('-s', metavar='services', type=str, nargs='+',
+                    help='services (i.e. service1 service2 ...)')
+
+# Execute the parse_args() method
+args = parser.parse_args()
+
+start(args.env, args.s, services)
